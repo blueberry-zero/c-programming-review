36d13b0876d03459b23ddd925fa6dc11f66b91c9
Saad Khan <khan.ssjc@gmail.com>
2024-09-15 23:57:01 -0400

    feat: exercises
    * complete chapter 1-3 notes
    * finish exercises in ch4 on content so far

---
+++ c-programming-introduction/01-hello-world/exercises.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) {
+
+  // exercise 1
+  printf("Hello World!\n");
+  printf("C Programming Rocks!\n");
+
+  char c = 'c';
+  int x = 1;
+  float f = 1.0F;
+  double d = 2.0F;
+
+
+  printf("%c\n", c);
+  printf("%d\n", x);
+  printf("%f\n", f);
+  printf("%f\n", d);
+  
+  return 0;
+}
--- c-programming-introduction/01-hello-world/main.c
+++ c-programming-introduction/01-hello-world/main.c
@@ -47,6 +47,46 @@ int main(void) {
   size_t char_size = sizeof(char_1);
   printf("The size of my char_1 object is %zu byte(s).\n", char_size);
 
-  printf("Done! Returning 0 and exiting\n!");
+
+  // integer types
+  // decimal representation
+  int int_1;
+  int_1 = 10;
+
+  // octal representation
+  int int_2;
+  int_2 = 012;
+
+  // hexadecimal representation
+  int int_3;
+  int_3 = 0xA;
+
+  printf("Decimal: %d\t Octal: %o\t Hexadecimal: %X\n", int_1, int_1, int_3);
+
+  //unsigned ints
+  unsigned int int_4 = -1U;
+  printf("Decimal unsigned int -1: %u\n", int_4);
+  printf("Size of unsigned int: %zu bytes\n", sizeof(int_4));
+
+  short short_1 = 32;
+  printf("Decimal short: %u\n", short_1);
+  printf("Size of short: %zu bytes\n", sizeof(short_1));
+
+  int x = 1;
+  short y = 2;
+  long z = 3L;
+  printf("The size of an short: %zu bytes\n", sizeof(y));
+  printf("The size of an int: %zu bytes\n", sizeof(x));
+  printf("The size of an long: %zu bytes\n", sizeof(z));
+
+
+  // floating point types
+  float float_1 = 123.45F;
+  printf("The size of a float: %zu bytes\n", sizeof(float_1));
+
+  double double_1 = 789.1234;
+  printf("The size of a double: %zu bytes\n", sizeof(double_1));
+   
+  printf("Done! Returning 0 and exiting!\n");
   return 0;
 }
