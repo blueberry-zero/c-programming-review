efdd58456410931426e0c3dd37fef899addb39e8
Saad Khan <khan.ssjc@gmail.com>
2024-09-28 10:23:01 -0400

    feat: basix exercises

--- c-programming-introduction/02-operators-expressions-statements/exercises.c
+++ c-programming-introduction/02-operators-expressions-statements/exercises.c
@@ -3,19 +3,10 @@
 int main(void) {
 
   // exercise 1
-  printf("Hello World!\n");
-  printf("C Programming Rocks!\n");
-
-  char c = 'c';
-  int x = 1;
-  float f = 1.0F;
-  double d = 2.0F;
-
-
-  printf("%c\n", c);
-  printf("%d\n", x);
-  printf("%f\n", f);
-  printf("%f\n", d);
+    int x = 123;
+    int y = 456;
+    int z = x + y;
+    printf("The sum of x: %d + y: %d is %d\n", x,y,z);
   
   return 0;
 }
